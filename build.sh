5b9fb84fa5f7a9be4be6d95df7600568cb615c3e
cristsr <styven21121@gmail.com>
2022-03-24 05:09:34 -0500

    feat: build script

--- build.sh
+++ build.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+echo "Pulling latest changes from GitHub"
+
 git pull
+
+echo "Install dependencies"
+
+npm run install
+
+echo "Build the project"
+
+npm run build
+
+echo "Stop the old server"
 
 docker stop rtmp-stream-app
 
+echo "Remove the old server"
+
 docker rm rtmp-stream-app
 
+echo "Build the new server"
+
 docker build -t rtmp-stream-app:latest .
+
+echo "Run the new server"
 
 docker run -d -p 1935:1935 -p 3000:3000 --name rtmp-stream-app rtmp-stream-app:latest
 
