7bb4bc91cd615d47862e88943d1d0e723dc8065e
cristsr <styven21121@gmail.com>
2022-04-22 15:04:04 -0500

    fix: update stream url on disconnect

--- src/stream/providers/stream.provider.ts
+++ src/stream/providers/stream.provider.ts
@@ -16,13 +16,13 @@ export class StreamProvider {
   }
 
   connectStream(data: StreamReq): Observable<void> {
-    const url = this.streamUrl.concat('/stream/connect');
+    const url = this.streamUrl.concat('/stream/connect/');
     this.logger.log(`Adding stream to ${url}`);
     return this.http.post(url, data).pipe(formatError());
   }
 
   disconnectStream(key: string): Observable<void> {
-    const url = this.streamUrl.concat('/stream/disconnect').concat(key);
+    const url = this.streamUrl.concat('/stream/disconnect/').concat(key);
     this.logger.log(`Removing stream from ${url}`);
     return this.http.delete(url).pipe(formatError());
   }
